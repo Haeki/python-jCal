46de572dadae7b8484eaef8e7e1bd90a69babde3
Mika Busch <haeki@hotmail.de>
2024-11-27 00:19:47 +0100

    Add boolean prop conversion and test all files in the asset folder

--- jCal/jCal.py
+++ jCal/jCal.py
@@ -4,7 +4,18 @@ import icalendar
 
 def convert_property(name, prop):
     name = name.lower()
-    params = dict(prop.params)
+    params = {k.lower(): v for k, v in prop.params.items() if k.lower()}
+    print("prop type", type(prop))
+    if value := params.pop('value', None):
+        if value.lower() == 'binary':
+            return [name, params, "binary", str(prop)]
+        if value.lower() == 'date':
+            val = prop.dt.isoformat()
+            return [name, params, "date", val]
+        if value.lower() == 'boolean':
+            return [name, params, "boolean", str(prop).lower() == 'true']
+    if isinstance(prop, icalendar.vBoolean):
+        return [name, params, "boolean", bool(prop)]
     if isinstance(prop, icalendar.vDDDTypes):
         dt = prop.dt
         if isinstance(dt, datetime):
@@ -23,8 +34,6 @@ def convert_property(name, prop):
         return [name, params, "text", str(prop)]
     if isinstance(prop, icalendar.vDate):
         val = prop.dt.isoformat()
-        if params.get('VALUE') == 'DATE':
-            params.pop('VALUE')
         return [name, params, "date", val]
     if isinstance(prop, icalendar.vDatetime):
         tzid = icalendar.prop.tzid_from_dt(dt)
@@ -34,7 +43,10 @@ def convert_property(name, prop):
         elif tzid:
             params.update({'TZID': tzid})
         return [name, params, "date-time", val]
-    return [name, params, "unknown", str(prop)]
+    if isinstance(prop, icalendar.vBinary):
+        return [name, params, "binary", str(prop)]
+    print(f"Unknown type: {type(prop)}")
+    return [name, params, type(prop), str(prop)]
 
 
 def to_jcal(comp: icalendar.Component):
--- tests/test_from_jCal.py
+++ tests/test_from_jCal.py
@@ -1,13 +1,21 @@
 import json
 import unittest
 from jCal.jCal import from_ical
+import os
 
 class TestFromJCal(unittest.TestCase):
 
     def test_valid(self):
-        with open("tests/assets/rfc.json", "r") as f:
-            jCal1 = json.load(f)
-        with open("tests/assets/rfc.ics", "r") as f:
-            jCal2 = from_ical(f.read())
-
-        self.assertListEqual(jCal1, jCal2)
+        for file in os.listdir("tests/assets"):
+            if not file.endswith(".ics"):
+                continue
+            file2 = file.replace(".ics", ".json")
+            if not os.path.exists(f"tests/assets/{file2}"):
+                continue
+            print("Testing", file)
+            with open(f"tests/assets/{file}", "r") as f:
+                jCal1 = from_ical(f.read())
+            with open(f"tests/assets/{file2}", "r") as f:
+                jCal2 = json.load(f)
+            print(jCal1)
+            self.assertListEqual(jCal2, jCal1)
